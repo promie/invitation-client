8ef0e232f6f4423d22b4c397a4b9626cb2d23679
Promie Yutasane <promie.yutasane@gmail.com>
2021-11-12 17:46:29 +1100

    Finalising mobile view

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,10 +4,13 @@ import Index from "../about";
 
 const heroContent = {
   heroImage: "img/hero/patrick-2.png",
-  heroMobileImage: "img-mobile-patrick",
+  heroMobileImage: "patrick-mobile",
   heroTitleName: "Patrick Yutasane",
   heroDesignation: "IS TURNING 1",
-  heroDescriptions: `You are invited to join the special day.`,
+  heroDescriptions: `PARTY INFO:`,
+  heroDate: 'SAT. DECEMBER 11TH AT 11AM',
+  heroLocation: 'STEEL PARK 531-565 ILLAWARRA ROAD, MARRICKVILLE',
+  heroRsvp: 'PATTY: 0466 247 568 | PROMIE 0423 702 138',
   heroBtn: "RSVP",
 };
 
@@ -30,20 +33,29 @@ const Hero = () => {
          />
         <div className="col-12 col-lg-8 offset-lg-4 home-details  text-center text-lg-start mt-lg-5 pt-lg-5">
           <div>
-            {/*<img*/}
-            {/*  src={`img/hero/${heroContent.heroMobileImage}.png`}*/}
-            {/*  className="img-fluid main-img-mobile d-sm-block d-lg-none"*/}
-            {/*  alt="hero man"*/}
-            {/*/>*/}
-            <h1 className="text-uppercase poppins-font">
-              {heroContent.heroTitleName}.
-              <span>{heroContent.heroDesignation}</span>
-            </h1>
-            <p className="open-sans-font">{heroContent.heroDescriptions}</p>
-            <button className="button" onClick={toggleModalOne}>
-              <span className="button-text">{heroContent.heroBtn}</span>
-              <span className="button-icon fa fa-arrow-right" />
-            </button>
+            <img
+              src={`img/hero/${heroContent.heroMobileImage}.png`}
+              className="img-fluid main-img-mobile d-sm-block d-lg-none"
+              alt="hero man"
+            />
+
+            <div className="info-wrapper">
+              <div className="info-container">
+                <h1 className="text-uppercase poppins-font">
+                  {heroContent.heroTitleName}.
+                  <span>{heroContent.heroDesignation}</span>
+                </h1>
+                <div className="open-sans-font bold">{heroContent.heroDescriptions}</div>
+                <div className="open-sans-font bold">{heroContent.heroDate}</div>
+                <div className="open-sans-font bold">{heroContent.heroLocation}</div>
+                <div className="open-sans-font mb-5 bold">{heroContent.heroRsvp}</div>
+                <button className="button" onClick={toggleModalOne}>
+                  <span className="button-text">{heroContent.heroBtn}</span>
+                  <span className="button-icon fa fa-arrow-right" />
+                </button>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
